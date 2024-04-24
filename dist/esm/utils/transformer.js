30fcff9bfe8ac7ba413aafd271b108aa7e32271d
Huub Segers <huub@bureau97.nl>
2024-04-24 18:49:57 +0200

    version 1.0.10-beta

--- dist/esm/utils/transformer.js
+++ dist/esm/utils/transformer.js
@@ -263,11 +263,15 @@ export class VtbDataTransformer {
         vtb_element.subtitle = element_data.subTitle;
         // set element description, get all contents from the <body> and remove all style attributes
         vtb_element.description = element_data.additionalText
-            ?.replace(this.re_body, '$1')
-            ?.replace(this.re_style, '');
+            ? element_data.additionalText
+                ?.replace(this.re_body, '$1')
+                ?.replace(this.re_style, '')
+            : '';
         vtb_element.additional_description = element_data.subAdditionalText
-            ?.replace(this.re_body, '$1')
-            ?.replace(this.re_style, '');
+            ? element_data.subAdditionalText
+                ?.replace(this.re_body, '$1')
+                ?.replace(this.re_style, '')
+            : '';
         vtb_element.optional = element_data.optional;
         vtb_element.price = parseFloat(element_data.olPrices?.salesTotal || 0);
         vtb_element.nights = element_data.flexNights || element_data.nights;

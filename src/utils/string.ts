6fff8cb390602f085d1865faa507792cc50b0115
Huub Segers <huub@bureau97.nl>
2024-10-15 17:41:51 +0200

    Add excludes on strip_tags

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,9 +1,16 @@
-export function strip_tags(html?: string) {
+
+export function strip_tags(html?: string, excludes?: Array<string>): string {
   if (!html) {
     return '';
   }
 
-  html = String(html).replace(/<\/?[^>]+(>|$)/g, '');
+  let excl = '';
+  if (excludes) {
+    excl = '(?:' + excludes.join('|') + ')\w*';
+  }
+
+  const re = new RegExp('<\/?' + excl + '[^>]*>', 'gi');
+  html = String(html).replace(re, '');
   html = html.replace(/&nbsp;/g, ' ');
   return html;
 }

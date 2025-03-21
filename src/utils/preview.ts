6abb1a65f8ee64b91f4274839089e6f22a1c9e54
Huub Segers <huub@bureau97.nl>
2025-03-21 14:06:12 +0100

    working on preview

--- src/utils/preview.ts
+++ src/utils/preview.ts
@@ -77,12 +77,16 @@ export class PreviewDataLoader {
           if (msg && msg.message && msg.message.fileName) {
             console.info(msg.message.fileName);
 
-            const file_url = encodeURIComponent(
-              `https://vtb-live-mode.s3.eu-west-1.amazonaws.com/${msg.message.fileName}`
-            );
-            const response = await fetch(
-              'http://localhost.b97.nl/vtb-preview-proxy/?url=' + file_url
-            );
+            // const file_url = encodeURIComponent(
+            //   `https://vtb-live-mode.s3.eu-west-1.amazonaws.com/${msg.message.fileName}`
+            // );
+            // const response = await fetch(
+            //   'http://localhost.b97.nl/vtb-preview-proxy/?url=' + file_url
+            // );
+            // const result = await response.json();
+
+            const file_url = `https://vtb-live-mode.s3.eu-west-1.amazonaws.com/${msg.message.fileName}`;
+            const response = await fetch(file_url);
             const result = await response.json();
 
             resolve(result.data);

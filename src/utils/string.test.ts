c0bf9b8f1f870746a59e49f383c90e878806523e
Huub Segers <huub@bureau97.nl>
2025-08-12 20:41:43 +0200

    add test framework and some tests

---
+++ src/utils/string.test.ts
@@ -0,0 +1,20 @@
+import { strip_tags } from './string';
+
+test('strip_tags', () => {
+  expect(strip_tags('<p>test</p>')).toBe('test');
+  expect(strip_tags('<p>test</p>', ['p'])).toBe('<p>test</p>');
+  expect(strip_tags('<p>test</p>', ['p', 'span'])).toBe('<p>test</p>');
+  expect(strip_tags('<p>test</p>', ['span'])).toBe('test');
+  expect(strip_tags('<p>test</p>', ['span', 'p'])).toBe('<p>test</p>');
+  expect(strip_tags('')).toBe('');
+  expect(strip_tags()).toBe('');
+  expect(strip_tags('test')).toBe('test');
+  expect(strip_tags('test', ['p'])).toBe('test');
+  expect(strip_tags('test', ['p', 'span'])).toBe('test');
+  expect(strip_tags('test', ['span'])).toBe('test');
+  expect(strip_tags('test', ['span', 'p'])).toBe('test');
+  expect(strip_tags('&nbsp;')).toBe(' ');
+  expect(strip_tags('<p><span>test</span>test</p>', ['span'])).toBe(
+    '<span>test</span>test'
+  );
+});

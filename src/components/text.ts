8062f46bde5e290e11ba4ed91bf2e431e7ab4b9a
Huub Segers <huub@bureau97.nl>
2025-09-16 22:53:07 +0200

    editor working again "ish"

--- src/components/text.ts
+++ src/components/text.ts
@@ -28,21 +28,36 @@ import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 // import {styleMap, StyleInfo} from 'lit/directives/style-map.js';
 
 import {
-  // Editor,
   InlineEditor,
-  Essentials,
+  Alignment,
+  Autosave,
   Bold,
-  Italic,
+  Essentials,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
   Heading,
-  Link,
+  Highlight,
+  HorizontalLine,
+  Indent,
+  IndentBlock,
+  Italic,
+  // Link,
+  List,
   Paragraph,
-  List
-  // Plugin,
-  // Command,
-  // ButtonView
+  PasteFromOffice,
+  RemoveFormat,
+  Strikethrough,
+  Subscript,
+  Superscript,
+  Underline
 } from 'ckeditor5';
 
-// import 'ckeditor5/ckeditor5.css';
+import type { EditorConfig } from 'ckeditor5';
+
+import 'ckeditor5/ckeditor5.css';
+
 
 // class VtbTextSaveCommand extends Command {
 //   override execute() {
@@ -106,6 +121,137 @@ export class VtbTextElement extends LitElement {
   protected editor?: InlineEditor = undefined;
   protected _destroy_timer?: ReturnType<typeof setTimeout>;
 
+  protected LICENSE_KEY = 'GPL';
+  protected editorConfig: EditorConfig = {
+    toolbar: {
+      items: [
+        'undo',
+        'redo',
+        '|',
+        'heading',
+        '|',
+        'fontSize',
+        'fontFamily',
+        'fontColor',
+        'fontBackgroundColor',
+        '|',
+        'bold',
+        'italic',
+        'underline',
+        'strikethrough',
+        'subscript',
+        'superscript',
+        'removeFormat',
+        '|',
+        'horizontalLine',
+        'highlight',
+        '|',
+        'alignment',
+        '|',
+        'bulletedList',
+        'numberedList',
+        'outdent',
+        'indent'
+      ],
+      shouldNotGroupWhenFull: false
+    },
+    plugins: [
+      Alignment,
+      Autosave,
+      Bold,
+      Essentials,
+      FontBackgroundColor,
+      FontColor,
+      FontFamily,
+      FontSize,
+      Heading,
+      Highlight,
+      HorizontalLine,
+      Indent,
+      IndentBlock,
+      Italic,
+      List,
+      Paragraph,
+      PasteFromOffice,
+      RemoveFormat,
+      Strikethrough,
+      Subscript,
+      Superscript,
+      Underline
+    ],
+    balloonToolbar: ['bold', 'italic', '|', 'link', '|', 'bulletedList', 'numberedList'],
+    fontFamily: {
+      supportAllValues: true
+    },
+    fontSize: {
+      options: [10, 12, 14, 'default', 18, 20, 22],
+      supportAllValues: true
+    },
+    heading: {
+      options: [
+        {
+          model: 'paragraph',
+          title: 'Paragraph',
+          class: 'ck-heading_paragraph'
+        },
+        {
+          model: 'heading1',
+          view: 'h1',
+          title: 'Heading 1',
+          class: 'ck-heading_heading1'
+        },
+        {
+          model: 'heading2',
+          view: 'h2',
+          title: 'Heading 2',
+          class: 'ck-heading_heading2'
+        },
+        {
+          model: 'heading3',
+          view: 'h3',
+          title: 'Heading 3',
+          class: 'ck-heading_heading3'
+        },
+        {
+          model: 'heading4',
+          view: 'h4',
+          title: 'Heading 4',
+          class: 'ck-heading_heading4'
+        },
+        {
+          model: 'heading5',
+          view: 'h5',
+          title: 'Heading 5',
+          class: 'ck-heading_heading5'
+        },
+        {
+          model: 'heading6',
+          view: 'h6',
+          title: 'Heading 6',
+          class: 'ck-heading_heading6'
+        }
+      ]
+    },
+    // initialData:
+    //   "<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou've successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What's next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor's\n\t\tconfiguration to match your application's style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don't hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<p>\n\t<i>An editor without the </i><code>Link</code>\n\t<i>plugin? That's brave! We hope the links below will be useful anyway </i>😉\n</p>\n<ul>\n\t<li>📝 Trial sign up: https://portal.ckeditor.com/checkout?plan=free,</li>\n\t<li>📕 Documentation: https://ckeditor.com/docs/ckeditor5/latest/installation/index.html,</li>\n\t<li>⭐️ GitHub (star us if you can!): https://github.com/ckeditor/ckeditor5,</li>\n\t<li>🏠 CKEditor Homepage: https://ckeditor.com,</li>\n\t<li>🧑‍💻 CKEditor 5 Demos: https://ckeditor.com/ckeditor-5/demo/</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser's\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n",
+    licenseKey: this.LICENSE_KEY,
+    link: {
+      addTargetToExternalLinks: true,
+      defaultProtocol: 'https://',
+      decorators: {
+        toggleDownloadable: {
+          mode: 'manual',
+          label: 'Downloadable',
+          attributes: {
+            download: 'file'
+          }
+        }
+      }
+    },
+    placeholder: 'Type or paste your content here!',
+    updateSourceElementOnDestroy: true
+  };
+
   @property({type: Boolean})
   editable: boolean = false;
 
@@ -116,21 +262,42 @@ export class VtbTextElement extends LitElement {
       return newVal !== oldVal;
     }
   })
-  contents: string | null = '';
+  contents: string = '';
 
   private get _editor(): HTMLElement | null {
-    return this.querySelector('div#editor-' + this.id);
+    console.debug('editor: ', this.renderRoot.querySelector('div#editor-' + this.id));
+    return this.renderRoot.querySelector('div#editor-' + this.id);
   }
 
   static override styles = css`
     :host {
       display: block;
+      margin: 0;
+      padding: 0;
+    }
+
+    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+    :root {
+      --ck-content-font-family: 'Lato';
+    }
+
+    .main-container {
+      font-family: var(--ck-content-font-family);
+      width: fit-content;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    .editor-container_inline-editor .editor-container__editor {
+      min-width: 795px;
+      max-width: 795px;
     }
   `;
 
   constructor() {
     super();
-    // console.debug('vtbtext:constructor');
+    console.debug('vtbtext:constructor');
     // create an id for the editor when no id is defined
     // this makes sure no conflicts can occur when
     // having multiple editors active at the same time
@@ -146,12 +313,12 @@ export class VtbTextElement extends LitElement {
     // it'll throw an error as soon as you try to
     // type anything in the editor.
     // the main error is coming from @ckeditor/ckeditor5-utils/src/dom/getborderwidths
-    // console.debug('vtbtext:createRenderRoot');
+    console.debug('vtbtext:createRenderRoot');
     return this;
   }
 
   override connectedCallback() {
-    // console.debug('vtbtext:connectedCallback');
+    console.debug('vtbtext:connectedCallback');
     super.connectedCallback();
 
     // copy innerHTML to the contents property
@@ -167,7 +334,7 @@ export class VtbTextElement extends LitElement {
   }
 
   override render() {
-    // console.debug('vtbtext:render');
+    console.debug('vtbtext:render');
 
     // TODO: check if innerHTML and content are the same,
     // if not copy contents to innerHTML
@@ -193,11 +360,11 @@ export class VtbTextElement extends LitElement {
   clickHandler(_e: Event) {
     // console.debug('vtbtext:clickHandler: ', _e);
 
-    // // console.debug('check innerHTML and content: ', {
-    //   innerHTML: this.innerHTML,
-    //   contents: this.contents,
-    //   'same?': boolean(this.innerHTML == this.contents),
-    // });
+    console.debug('check innerHTML and content: ', {
+      innerHTML: this.innerHTML,
+      contents: this.contents,
+      'same?': (this.innerHTML == this.contents),
+    });
 
     if (this.isEditorInitialized && this._destroy_timer) {
       // console.debug('clear editor destruction timer');
@@ -206,7 +373,7 @@ export class VtbTextElement extends LitElement {
     }
 
     if (this.editable && !this.isEditorInitialized) {
-      // console.debug('initializing editor');
+      console.debug('initializing editor');
 
       if (!this._editor) {
         console.warn('not initializing the editor, editor is null');
@@ -216,41 +383,17 @@ export class VtbTextElement extends LitElement {
       // set the inialized bit..
       this.isEditorInitialized = true;
 
-      InlineEditor.create(this._editor, {
-        updateSourceElementOnDestroy: true,
-        // every tool has a plugin!
-        plugins: [
-          // VtbTextSave,
-          Essentials,
-          Heading,
-          Bold,
-          Italic,
-          Link,
-          List,
-          Paragraph
-          // Autosave,
-        ],
-        // these are the available tools
-        toolbar: [
-          // 'save',
-          '|',
-          'heading',
-          '|', // separator!
-          'bold',
-          'italic',
-          'link',
-          'bulletedList',
-          'numberedList',
-          'undo',
-          'redo'
-        ]
-      })
+      const currentConfig = {...this.editorConfig};
+      currentConfig.initialData = this.innerHTML || '';
+
+      InlineEditor
+        .create(this._editor, currentConfig)
         .then((editorInstance) => {
-          // console.debug('promise:then');
+          console.debug('promise:then');
           this.editor = editorInstance;
 
           if (!this.editor) {
-            // console.debug('no editor (yet)');
+            console.debug('no editor (yet)');
             return;
           }
 
@@ -259,7 +402,7 @@ export class VtbTextElement extends LitElement {
           this.editor.ui.focusTracker.on(
             'change:isFocused',
             (_evt, _name, isFocused) => {
-              // console.debug('focus changed');
+              console.debug('focus changed');
               if (!isFocused) {
                 this.lostFocus();
               }
@@ -270,21 +413,21 @@ export class VtbTextElement extends LitElement {
           this.editor.focus();
         })
         .catch((error) => {
-          // console.debug('received error');
+          console.debug('received error');
           console.error(error.stack);
         });
     }
   }
 
   protected lostFocus() {
-    // console.debug('vtbtext:lostFocus');
+    console.debug('vtbtext:lostFocus');
 
     if (!this.editor) {
-      // console.debug('no editor (yet)');
+      console.debug('no editor (yet)');
       return;
     }
 
-    // console.debug('changed data: ', this.editor.getData());
+    console.debug('changed data: ', this.editor.getData());
 
     // dispatch custom event
     const changed_content = this.editor.getData();
@@ -294,22 +437,34 @@ export class VtbTextElement extends LitElement {
       },
       bubbles: true
     });
-    // console.debug('dispatching change event: ');
+    console.debug('dispatching change event: ');
     this.dispatchEvent(event);
 
     // schedule destroying the editor after losing focus
     // console.debug('schedule destroy');
     const destroy = this._destroyEditor.bind(this);
     this._destroy_timer = setTimeout(destroy, 3000);
+
+
     // }
   }
 
   protected _destroyEditor() {
-    // console.debug('destroying editor..');
+    console.debug('destroying editor..');
     if (this.editor) {
+      // const changed_content = this.editor.getData();
+
+      // this._editor.style.display = 'none';
+
       this.editor.destroy();
       this.isEditorInitialized = false;
       delete this.editor;
+
+      // if (this._editor){
+      //   console.debug('set innerHTML: ', changed_content);
+      //   this._editor.innerHTML = changed_content;
+      // }
+
     }
   }
 }
